60a737369f819706cda91fa4d5de8b8efb7da409
JCamp-12 <username@users.noreply.github.com>
2017-10-24 17:23:37 -0400

    pseudocode for heaps alg and factorials identified

---
+++ ML.js
@@ -0,0 +1,28 @@
+// procedure generate(n : integer, A : array of any):
+if (n === 1) {
+  return A
+} else {
+  for(let i = 0; i < n-1; i++)
+}
+
+
+
+
+
+
+
+
+// procedure generate(n : integer, A : array of any):
+// if n = 1 then
+//       output(A)
+// else
+//     for i := 0; i < n - 1; i += 1 do
+//         generate(n - 1, A)
+//         if n is even then
+//             swap(A[i], A[n-1])
+//         else
+//             swap(A[0], A[n-1])
+//         end if
+//     end for
+//     generate(n - 1, A)
+// end if
