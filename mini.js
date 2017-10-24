97ecbde335e6d415f6b15527a1d2aef539933ece
mixelpixel <mixelpix@mac.com>
2017-10-24 14:44:34 -0400

    minor

--- mini.js
+++ mini.js
@@ -1,13 +1,14 @@
+'use-strict';
+/* eslint no-console: 0 */
 /*
 Mini lab
-Write code in your language of choice to generate
-all permutations of the following sets:
+Write code in your language of choice to generate all permutations of the following sets:
 (1, 2, 3)
 ('a', 'b', 'c', 'd')
 ('corn', 'turkey', 'winter', 'algebra', 'window')
 e.g.
-!3: 123, 132, 213, 231, 312, 321 = 6 permutations
-!4: abcd, abdc, acbd, acdb, adbc, adcb, etc... = 24 permutations
+!3: 123, 132, 213, 231, 312, 321                    = 6 permutations
+!4: abcd, abdc, acbd, acdb, adbc, adcb, etc...      = 24 permutations
 !5: (corn, turkey, winter, algebra, window),
     (corn, turkey, winter, window, algebra), etc... = 120 permutations
 */
@@ -15,10 +16,10 @@ e.g.
 const perm = (...args) => {
   // edge case
   if (args.length === 0) {
-    return 'Ya gotta pass in some args, bruh.'
+    return 'Ya gotta pass in some args, bruh.';
   // BASE case
   } else if (args.length === 1) {
-    return args[0]
+    return args[0];
   }
 
   let i, j;
@@ -29,13 +30,13 @@ const perm = (...args) => {
   for (i = 0; i < args.length; i++) {
     // console.log(i, args[i]);
     // console.log(args.slice(1, args.length));
-    sub = perm(args.slice(i + 1, args.length))
+    const sub = perm(args.slice(i + 1, args.length));
     for (j = 0; j < args[0].length; j++) {
-      permutations.push(sub[i])
+      permutations.push(sub[i]);
     }
   }
   return permutations;
-}
+};
 
 console.log(perm());
 console.log(perm('Scooby Snacks'));
@@ -44,15 +45,15 @@ console.log(perm('a', 'b', 'c', 'd'));
 console.log(perm('corn', 'turkey', 'winter', 'algebra', 'window'));
 
 
-// const allAnagrams = (str, start = '') => {
-//   if (str.length === 1) return [start + str];
-//   const anagrams = [];
-//
-//   for (let i = 0; i < str.length; i++) {
-//     const result = allAnagrams(str.substr(0, i) + str.substr(i + 1), str[i]);
-//     for (let j = 0; j < result.length; j++) {
-//       anagrams.push(start + result[j]);
-//     }
-//   }
-//   return anagrams;
-// };
+const allAnagrams = (str, start = '') => {
+  if (str.length === 1) return [start + str];
+  const anagrams = [];
+
+  for (let i = 0; i < str.length; i++) {
+    const result = allAnagrams(str.substr(0, i) + str.substr(i + 1), str[i]);
+    for (let j = 0; j < result.length; j++) {
+      anagrams.push(start + result[j]);
+    }
+  }
+  return anagrams;
+};
