f10b407726a7ceee2d89c6f9d81aa57bba683006
Huck <evanallen51@gmail.com>
2017-10-25 15:28:53 -0500

    Implemented Exhaustive search and helper function
    1. The possiblePerm function prints the number of possible permutations and returns the length of the array which is used to create the n variable in the perm function
    2. The swap function swaps two values in an array. It is used as a helper function in the perm function
    3. The perm function finds every possible permutation of an array and prints it to the console. It also calculates the distance between each city using the measureDistance helper function. It then sums the distances and compares the sum to the current fastest route and saves the new route if it is faster than the current fastest route. perm implements the helper functions possiblePerm, swap, and measureDistance
    4. The function measureDistance is built in its own file and imported into permutatuions.js. It measures the distance between two cities using the distance formula

---
+++ permutations.js
@@ -0,0 +1,84 @@
+const measureDistance = require('./measureDistance');
+
+// Test data sets
+// 6 possible permutations
+const data1 = [1, 2, 3];
+// 24 possible permutations
+const data2 = ['a', 'b', 'c', 'd'];
+// 120 possible permutations
+const data3 = ['corn', 'turkey', 'winter', 'algebra', 'window'];
+
+// Real data
+const cities = [
+  {name: "Denver", x: 500, y: 500},
+  {name: "Salt Lake City", x: 300, y: 500},
+  {name: "Cheyenne", x: 500, y: 600},
+  {name: "Santa Fe", x: 500, y: 350},
+  // {name: "Minneapolis", x: 1200, y: 1450}
+];
+const fastestRoute = [];
+let fast = Number.MAX_VALUE;
+
+// Finds number of possible permutations for a given data set
+const possiblePerm = (arr) => {
+  const len = arr.length;
+  let possible = len;
+
+  for(let i = len - 1; i > 0; i--) {
+    possible *= i;
+  }
+  console.log('Possible permutations: ' + possible);
+  return len;
+}
+
+// Swaps values in an array
+const swap = (arr, x, y) => {
+  const temp = arr[x];
+  arr[x] = arr[y];
+  arr[y] = temp;
+}
+
+// Prints each possible permutation of a given data set
+const perm = (arr, n = 0) => {
+  let temp;
+  const tarr = [];
+
+  if (n === 0) {
+    n = possiblePerm(arr);
+  }
+
+  if (n === 1) {
+    console.log(arr);
+    for(let j = 0; j < arr.length-1; j++) {
+      tarr.push(measureDistance(arr[j], arr[j+1]));
+    }
+
+    tarr.push(measureDistance(arr[arr.length-1], arr[0]));
+    result = tarr.reduce((sum, value) => sum + value);
+
+    if (result < fast) {
+      fast = result;
+      fastestRoute.length = 0;
+      for(let i = 0; i < arr.length; i++) {
+        fastestRoute.push(arr[i].name);
+      }
+    }
+  } else {
+    for(let i = 0; i < n - 1; i++) {
+      perm(arr, n - 1);
+      if (n % 2 === 0) {
+        swap(arr, i, n - 1);
+      } else {
+        swap(arr, 0, n - 1);
+      }
+    }
+    perm(arr, n - 1);
+  }
+}
+
+perm(cities);
+console.log("FAST: " + fast);
+console.log("Fastest route: " + fastestRoute);
+// perm(data1);
+// perm(data2);
+// perm(data3);
