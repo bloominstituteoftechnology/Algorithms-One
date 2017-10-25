eba8f308d417ca5fc6bf78ecefe830105f0d79af
mixelpixel <mixelpix@mac.com>
2017-10-24 22:18:43 -0400

    on hold

--- exhaustiveSearch.js
+++ exhaustiveSearch.js
@@ -9,9 +9,11 @@ const cities = require('./cities');
 const Cities = cities.Cities;
 const distance = require('./distance');
 const distanceBetween = distance.distanceBetween;
+const count = require('./heapsAlgo');
+const countSet = count.countSet;
 const MAX_SAFE_NUMBER = 9007199254740991;
 
-console.log(MAX_SAFE_NUMBER);
+console.log('max safe number:', MAX_SAFE_NUMBER);
 
 // console log each city's coordinates
 for (let i = 0; i < Cities.length; i++) {
@@ -20,3 +22,6 @@ for (let i = 0; i < Cities.length; i++) {
 
 // console log the distance between two cities
 console.log(distanceBetween(Cities[0], Cities[2]));
+
+// enumerate the permutations
+console.log(countSet([0, 1, 2, 3]));
