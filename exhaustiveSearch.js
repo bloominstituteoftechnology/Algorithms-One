e7935de61998e96d501539ec0b19fbef4f310029
mixelpixel <mixelpix@mac.com>
2017-10-24 20:04:40 -0400

    Cities array of objects & Distance calculation √

--- exhaustiveSearch.js
+++ exhaustiveSearch.js
@@ -7,22 +7,33 @@
  *
  ******************************************************************************/
 
-const Cities = {
-  Denver: {x: 500, y:500},
-  SaltLakeCity: {x: 300, y:500},
-  Cheyenne: {x: 500, y:600},
-  SantaFe: {x: 500, y:350}
-};
+const Cities = [
+  {
+    name:'Denver',
+    x:500,
+    y:500},
+  {
+    name:'Salt Lake City',
+    x:300,
+    y:500},
+  {
+    name:'Cheyenne',
+    x:500,
+    y:600},
+  {
+    name:'Santa Fe',
+    x:500,
+    y:350}
+];
 
-console.log('Denver:       ', Cities.Denver.x, Cities.Denver.y);
-console.log('SaltLakeCity: ', Cities.SaltLakeCity.x, Cities.SaltLakeCity.y);
-console.log('Cheyenne:     ', Cities.Cheyenne.x, Cities.Cheyenne.y);
-console.log('SantaFe:      ', Cities.SantaFe.x, Cities.SantaFe.y);
+for (let i = 0; i < Cities.length; i++) {
+  console.log(`${Cities[i].name}: ${Cities[i].x}, ${Cities[i].y}`);
+}
 
 // Calcukate the distance between two points:
 const distanceBetween = (city1, city2) => {
-  console.log(`The distance between ${city1} & ${city2}:`);
+  console.log(`The distance between ${city1.name} & ${city2.name}:`);
   return Math.sqrt(Math.pow(city1.x - city2.x, 2) + Math.pow(city1.y - city2.y, 2));
 };
 
-console.log(distanceBetween(Cities.Denver, Cities.Cheyenne));
+console.log(distanceBetween(Cities[0], Cities[2]));
