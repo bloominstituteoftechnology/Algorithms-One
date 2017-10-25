840d10d8311c427faebdcd086ed95d7aa5f9f71f
mixelpixel <mixelpix@mac.com>
2017-10-25 14:46:47 -0400

    idem

--- cities.js
+++ cities.js
@@ -9,20 +9,24 @@ const Cities = [
   {
     name:'Denver',
     x:500,
-    y:500},
+    y:500,
+  },
   {
     name:'Salt Lake City',
     x:300,
-    y:500},
+    y:500,
+  },
   {
     name:'Cheyenne',
     x:500,
-    y:600},
+    y:600,
+  },
   {
     name:'Santa Fe',
     x:500,
     y:350
   },
+
 ];
 
 module.exports = {
