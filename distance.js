90132ba1751c15310b17da8771e3877b9a2b71ec
mixelpixel <mixelpix@mac.com>
2017-10-24 22:18:06 -0400

    remove console.log

--- distance.js
+++ distance.js
@@ -7,7 +7,7 @@
 /* eslint no-console: 0 */
 
 const distanceBetween = (city1 = {}, city2 = {}) => {
-  console.log(`The distance between ${city1.name} & ${city2.name}:`);
+  // console.log(`The distance between ${city1.name} & ${city2.name}:`);
   const x = Math.pow(city1.x - city2.x, 2);
   const y = Math.pow(city1.y - city2.y, 2);
   return Math.sqrt(x + y);
