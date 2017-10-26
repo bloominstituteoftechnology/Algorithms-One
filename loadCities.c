f0f19f1c51269790c8c2d69514bc2f40d77f6f0e
wlharvey4 <pinecone062@gmail.com>
2017-10-26 11:31:22 -0700

    loadCities ver 0.2_b: Refactored code into a library with a header file

--- loadCities.c
+++ loadCities.c
@@ -2,66 +2,16 @@
  * loadCities.c
  * 2017-10-25
  *
- * Version 0.2_a
+ * Version 0.2_b
  *
  * Load usa115475_cities_a.txt and usa115475.tsp
  * format for use by Traveling Salesman Problem
  */
 
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-#include <ctype.h>
-#include <math.h>
+#include "./loadCities.h"
 
-#define CITIES "./TSP/usa115475.tsp"
-/* NOTE: usa115475_cities.txt must first be processed by cityfilter.pl
-         to produce usa115475_cities_a.txt so that the sscan library
-         function will work properly */
-#define CITIES_NAMES "./TSP/usa115475_cities_a.txt"
-#define CITIES_SIZE 115475
-#define READ "r"
-#define BUFSIZE 100
-
-extern int errno;
-
-struct City {
-  char *name;
-  char *state;
-  double x;
-  double y;
-};
-
-/********* PROTOTYPES ***********/
-
-/* Find the first line at which cities begins,
- * i.e., skip over header lines */
-void
-findFirstCity(FILE *);
-
-/* Get the next city line; returns EOF
- * on eof or 1 on successful read */
-int
-getCity(FILE *, char *);
-
-/* Given a line containing city coordinates,
- * and a line containing a city and state,
- * fill a City struct */
-void
-fillCity(char *, char *, struct City *);
-
-/* Given a line, extract its coordinates */
-double
-getCoord(char *);
-
-/* Print a city's name, state, and x and y coordinates */
-void
-printCityInfo(struct City *);
-
-/********************************/
-
-int
-main(int argc, char *argv[]) {
+struct City **
+loadCities(void) {
   /* open the two files for reading */
   FILE *cities = fopen(CITIES, READ);
   FILE *cities_names = fopen(CITIES_NAMES, READ);
@@ -80,7 +30,7 @@ main(int argc, char *argv[]) {
   char city_name[BUFSIZE];
   int result;
 
-  struct City *cities_arr[CITIES_SIZE];
+  struct City **cities_arr = malloc(sizeof(struct City) * CITIES_SIZE);
   int city_index = 0;
 
   // add city data to structs
@@ -103,6 +53,9 @@ main(int argc, char *argv[]) {
     fprintf(stderr, "Error\n");
 
   printf("successfully loaded city data\n");
+
+  return cities_arr;
+
   /* NOTE: free city data at conclusion */
 
 } /***************************************************/
---
+++ loadCities.h
@@ -0,0 +1,69 @@
+/*
+ * loadCities.h
+ * 2017-10-25
+ *
+ * Version 0.2_b
+ *
+ * Load usa115475_cities_a.txt and usa115475.tsp
+ * format for use by Traveling Salesman Problem
+ */
+
+#ifndef LOADCITIES
+#define LOADCITIES
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <math.h>
+
+#define CITIES "./TSP/usa115475.tsp"
+/* NOTE: usa115475_cities.txt must first be processed by cityfilter.pl
+         to produce usa115475_cities_a.txt so that the sscan library
+         function will work properly */
+#define CITIES_NAMES "./TSP/usa115475_cities_a.txt"
+#define CITIES_SIZE 115475
+#define READ "r"
+#define BUFSIZE 100
+
+#endif
+
+extern int errno;
+
+struct City {
+  char *name;
+  char *state;
+  double x;
+  double y;
+};
+
+/********* PROTOTYPES ***********/
+
+struct City **
+loadCities(void);
+
+/* Find the first line at which cities begins,
+ * i.e., skip over header lines */
+void
+findFirstCity(FILE *);
+
+/* Get the next city line; returns EOF
+ * on eof or 1 on successful read */
+int
+getCity(FILE *, char *);
+
+/* Given a line containing city coordinates,
+ * and a line containing a city and state,
+ * fill a City struct */
+void
+fillCity(char *, char *, struct City *);
+
+/* Given a line, extract its coordinates */
+double
+getCoord(char *);
+
+/* Print a city's name, state, and x and y coordinates */
+void
+printCityInfo(struct City *);
+
+/********************************/
+
